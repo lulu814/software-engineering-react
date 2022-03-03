bf69e360d77d4f16c22f5fe675ab0e861d001b17
Lu Liu <luliu@Lus-MacBook-Pro.local>
2022-03-03 00:02:11 -0500

    split user-list into two files as mock axios will affect real REST API

---
+++ src/tests/user-list-mock.test.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+import {findAllUsers} from "../services/users-service";
+import {render, screen} from "@testing-library/react";
+import {HashRouter} from "react-router-dom";
+import {UserList} from "../components/profile/user-list";
+
+const MOCKED_USERS = [
+    {username: 'ellen_ripley', password: 'lv426', email: 'repley@weyland.com', _id: "123"},
+    {username: 'sarah_conor', password: 'illbeback', email: 'sarah@bigjeff.com', _id: "234"},
+]
+jest.mock('axios');
+
+// test rendering from Mock
+test('user list renders mocked', async () => {
+    // simulate response from REST with static response
+    axios.get.mockImplementation(() =>
+        Promise.resolve({ data: {users: MOCKED_USERS} }));
+    const response = await findAllUsers();
+    // retrieve users from Mock
+    const users = response.users;
+
+    // render users retrieved from
+    render(
+        <HashRouter>
+            <UserList users={users}/>
+        </HashRouter>);
+    // verify user appears in screen somewhere
+    const user = screen.getByText(/ellen_ripley/i);
+    expect(user).toBeInTheDocument();
+});
--- src/tests/user-list.test.js
+++ src/tests/user-list.test.js
@@ -2,13 +2,13 @@ import {UserList} from "../components/profile/user-list";
 import {screen, render} from "@testing-library/react";
 import {HashRouter} from "react-router-dom";
 import {findAllUsers} from "../services/users-service";
-import axios from "axios";
 
+// sample users rendered by UserList
 const MOCKED_USERS = [
   {username: 'ellen_ripley', password: 'lv426', email: 'repley@weyland.com', _id: "123"},
   {username: 'sarah_conor', password: 'illbeback', email: 'sarah@bigjeff.com', _id: "234"},
 ]
-
+// test rendering user array render a user array
 test('user list renders static user array', () => {
   render(
     <HashRouter>
@@ -18,6 +18,7 @@ test('user list renders static user array', () => {
   expect(linkElement).toBeInTheDocument();
 });
 
+// test rendering from REST retrieve users from REST render users retrieved from REST API
 test('user list renders async', async () => {
   const users = await findAllUsers();
   render(
@@ -27,19 +28,3 @@ test('user list renders async', async () => {
   const linkElement = screen.getByText(/spacex/i);
   expect(linkElement).toBeInTheDocument();
 })
-
-// jest.mock('axios');
-test('user list renders mocked', async () => {
-  axios.get.mockImplementation(() =>
-    Promise.resolve({ data: {users: MOCKED_USERS} }));
-  const response = await findAllUsers();
-  const users = response.users;
-
-  render(
-    <HashRouter>
-      <UserList users={users}/>
-    </HashRouter>);
-
-  const user = screen.getByText(/ellen_ripley/i);
-  expect(user).toBeInTheDocument();
-});
