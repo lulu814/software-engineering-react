e5b93d0e312271a15f39d48bc9ca54ce45dc5da8
Lu Liu <luliu@Lus-MacBook-Pro.local>
2022-03-27 17:06:35 -0400

    add dislike services into like-service file and create my-dislikes

--- src/services/likes-service.js
+++ src/services/likes-service.js
@@ -24,6 +24,14 @@ export const tuitLikedByMe = (uid, tid) =>
     api.get(`${USERS_API}/${uid}/likes/${tid}`)
         .then(response => response.data);
 
+export const findAllTuitsDislikedByUser = (userId) =>
+    api.get(`${USERS_API}/${userId}/dislikes`)
+        .then(response => response.data);
+
+export const findAllUsersThatDislikedTuit = (tid) =>
+    api.get(`${TUITS_API}/${tid}/dislikes`)
+        .then(response => response.data);
+
 export const userDislikesTuit = (uid, tid) =>
     api.put(`${USERS_API}/${uid}/dislikes/${tid}`)
         .then(response => response.data);
